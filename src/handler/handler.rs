6f04b1a2f0d3eaf2ed27336a7f7f8fc9e14eea45
Mickael972 <madallstar@gmail.com>
2025-10-03 12:07:23 +0200

    Permettre la séléction dynamique des cryptos via query params

--- src/handler/handler.rs
+++ src/handler/handler.rs
@@ -1,9 +1,20 @@
 use crate ::service::coingecko_service;
 use actix_web::{get, HttpResponse, Responder};
+use  serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct CryptoQuery {
+    #[serde(default = "default_ids")]
+    id: String,
+}
+
+fn default_ids() -> String {
+    "bitcoin,ethereum".to_string()
+}
 
 #[get("/crypto")]
-pub async fn get_crypto() -> impl Responder {
-    match  coingecko_service::fetch_crypto_data().await {
+pub async fn get_crypto(query: actix_web::web::Query<CryptoQuery>) -> impl Responder {
+    match coingecko_service::fetch_crypto_data(&query.id).await {
         Ok(crypto) => HttpResponse::Ok().json(crypto),
         Err(_) => HttpResponse::InternalServerError().body("Erreur lors de la récupération des données."),
     }
--- src/service/coingecko_service.rs
+++ src/service/coingecko_service.rs
@@ -1,10 +1,10 @@
 use crate::models::crypto::Crypto;
 use reqwest::{header, Error};
 
-pub async fn fetch_crypto_data() -> Result<Vec<Crypto>, Error> {
+pub async fn fetch_crypto_data(ids: &str) -> Result<Vec<Crypto>, Error> {
     let client = reqwest::Client::new();
     let cryptos = client
-        .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+        .get(&format!("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={}", ids))
         .header(header::USER_AGENT, "Mozilla/5.0 (compatible; MyRustApp/1.0; +http://mywebsite.com)")
             .send()
             .await?
