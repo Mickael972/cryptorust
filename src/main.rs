997bf59020242b4af357db68922431f0053253d8
Mickael972 <madallstar@gmail.com>
2025-10-03 10:10:05 +0200

    Implémentation de la récupération des données de CoinGecko

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
-mod models; // On importe le module models (même si on ne l'utilise pas encore).
+mod models; 
 mod handler;
 mod service;
 use actix_web::{get, App, HttpServer, Responder};
 
 
-// C'est une fonction qui répondra quand on visite la page d'accueil.
-// L'étiquette #[get("/")] dit : "Hé, si quelqu'un arrive sur l'adresse de base, c'est toi qui travailles !"
+
 #[get("/")]
 async fn index() -> impl Responder {
     "Bonjour ! Le serveur est en ligne."
 }
 
-// C'est la fonction principale qui lance tout.
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("🚀 Le serveur démarre sur http://127.0.0.1:8080");
 
-    // On crée un nouveau serveur HTTP.
+    
     HttpServer::new(|| {
-        // On enregistre notre fonction "index" pour qu'elle réponde à la racine "/".
+        
         App::new().service(handler::handler::get_crypto)
     })
-    .bind(("127.0.0.1", 8080))? // On dit au serveur d'écouter sur le port 8080.
+    .bind(("127.0.0.1", 8080))? 
     .run()
     .await
 }
